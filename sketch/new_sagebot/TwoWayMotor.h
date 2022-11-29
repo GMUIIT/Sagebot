90b4a37992c5b832ddd12df7d71717c5e4f143e1
Dylan Jones <dylanjones2011@gmail.com>
2022-11-29 18:59:25 -0500

    Add TwoWayMotor.h for usage in future sketches

---
+++ sketch/new_sagebot/TwoWayMotor.h
@@ -0,0 +1,35 @@
+class TwoWayMotor {
+  private:
+    int dirPinA, dirPinB, enPin;
+  public:
+    TwoWayMotor(int dirPinA, int dirPinB, int enPin):
+      dirPinA(dirPinA), dirPinB(dirPinB), enPin(enPin) {
+      }
+
+      void init() {
+        pinMode(dirPinA, OUTPUT);
+        pinMode(dirPinB, OUTPUT);
+        pinMode(enPin, OUTPUT);
+        digitalWrite(dirPinA, LOW);
+        digitalWrite(dirPinB, LOW);
+        digitalWrite(enPin, LOW);
+      }
+
+      void write(int amount){
+        if (amount < -255 || amount > 255) {
+          return;
+        }
+        if (amount < 0) {
+          digitalWrite(dirPinA, HIGH);
+          digitalWrite(dirPinB, LOW);
+          amount *= -1;
+        } else if (amount > 0) {
+          digitalWrite(dirPinA, LOW);
+          digitalWrite(dirPinB, HIGH);
+        } else {
+          digitalWrite(dirPinA, LOW);
+          digitalWrite(dirPinB, LOW);
+        }
+        analogWrite(enPin, amount);
+      }
+};
